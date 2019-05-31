ba8288251a67b7b01fdb98307402e99fa2db7b1f
Mauri05 <mauriagustin05@gmail.com>
2019-05-31 20:01:32 -0300

    main view

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -10,7 +10,7 @@
             <!-- Grid column -->
             <div class="col-md-2 mb-3">
                 <h6 class="text-uppercase font-weight-bold">
-                    <a href="#!" class="text-warning">Sobre Nosotros</a>
+                    <a href="#!" class="text-warning"><i class="fa fa-building" aria-hidden="true"></i> Sobre Nosotros</a>
                 </h6>
             </div>
             <!-- Grid column -->
@@ -19,7 +19,7 @@
             <!-- Grid column -->
             <div class="col-md-2 mb-3">
                 <h6 class="text-uppercase font-weight-bold">
-                    <a href="#!" class="text-warning">Contacto</a>
+                    <a href="#!" class="text-warning"><i class="fas fa-phone"></i> Contacto</a>
                 </h6>
             </div>
             <!-- Grid column -->
@@ -27,7 +27,7 @@
             <!-- Grid column -->
             <div class="col-md-2 mb-3">
                 <h6 class="text-uppercase font-weight-bold">
-                    <a href="#!" class="text-warning">Información</a>
+                    <a href="#!" class="text-warning"><i class="fas fa-info-circle"></i> Información</a>
                 </h6>
             </div>
             <!-- Grid column -->
@@ -61,23 +61,16 @@
                 <div class="mb-5 flex-center text-center">
 
                     <!-- Facebook -->
-                    <a class="fb-ic" href="#" style="color: #3b5998; font-size: 40px">
+                    <a class="fb-ic btn" href="https://www.facebook.com" style="color: #3b5998; font-size: 40px">
                         <i class="fab fa-facebook-f fa-lg white-text mr-4"> </i>
                     </a>
                     <!-- Twitter -->
-                    <a class="tw-ic" href="#" style="color: #1da1f2; font-size: 40px">
+                    <a class="tw-ic btn" href="https://www.twitter.com" style="color: #1da1f2; font-size: 40px">
                         <i class="fab fa-twitter fa-lg white-text mr-4"> </i>
                     </a>
-                    <!-- Google +-->
-                    <a class="gplus-ic" href="#" style="color: #ff5733; font-size: 40px">
-                        <i class="fab fa-google-plus-g fa-lg white-text mr-4"> </i>
-                    </a>
-                    <!--Linkedin -->
-                    <a class="li-ic" href="#" style="color: #0e76a8; font-size: 40px">
-                        <i class="fab fa-linkedin-in fa-lg white-text mr-4"> </i>
-                    </a>
+
                     <!--Instagram-->
-                    <a class="ins-ic" href="#" style="color: #8a3ab9; font-size: 40px">
+                    <a class="ins-ic btn" href="https://www.instagram.com/?hl=es-la" style="color: #8a3ab9; font-size: 40px">
                         <i class="fab fa-instagram fa-lg white-text mr-4"> </i>
                     </a>
                 </div>
@@ -94,7 +87,7 @@
     <!-- Copyright -->
     <hr class="rgba-white-light" style="margin: 0 15%;">
     <div class="footer-copyright text-center py-3">
-        <a href="#" class="text-warning"> CampusMaster.com</a>
+        <a href="#" class="text-warning"><i class="far fa-copyright"></i> CampusMaster.com - 2019</a>
     </div>
     <!-- Copyright -->
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,96 +1,89 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>CampusMaster</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
-            .full-height {
-                height: 100vh;
-            }
+    <!-- Fonts -->
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 
-            .position-ref {
-                position: relative;
-            }
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
 
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-
-            @if (Route::has('login')  )
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md" size="10">
-                    CampusMaster
-                </div>
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                </div>
+    @include('layouts.notlogued')
+    <div class="container-materias">
+        <div class="card-materias border" style="width: 18rem;">
+            <img class="card-img-top" src="http://www.comunicacionesua.cl/wp-content/uploads/2018/11/coloquio-matematica.jpeg" alt="Card image cap">
+            <div class="card-body text-center">
+                <h5 class="card-title">Matemática II</h5>
+                <p class="card-text">Aqui su descripción</p>
+                <a href="#" class="btn btn-primary"> <i class="fas fa-info-circle"></i>  Mas información</a>
             </div>
         </div>
+        <div class="card-materias border" style="width: 18rem;";>
+            <img class="card-img-top" src="https://i.blogs.es/389033/programming/450_1000.jpg" alt="Card image cap">
+            <div class="card-body text-center">
+                <h5 class="card-title">Laboratorio V</h5>
+                <p class="card-text">Aqui su descripción</p>
+                <a href="#" class="btn btn-primary"> <i class="fas fa-info-circle"></i>  Mas información</a>
+            </div>
+        </div>
+        <div class="card-materias border" style="width: 18rem;";>
+            <img class="card-img-top" src="https://www.ipc3.com/wp-content/uploads/programming-java.jpg" alt="Card image cap">
+            <div class="card-body text-center">
+                <h5 class="card-title">Programación Avanzada I</h5>
+                <p class="card-text">Aqui su descripción</p>
+                <a href="#" class="btn btn-primary"> <i class="fas fa-info-circle"></i>  Mas información</a>
+            </div>
+        </div>
+        <div class="card-materias border" style="width: 18rem;";>
+            <img class="card-img-top" src="https://i.pinimg.com/originals/8e/e7/3b/8ee73b4b283b0dda5b415e2f2da7a254.jpg" alt="Card image cap">
+            <div class="card-body text-center">
+                <h5 class="card-title">Redes</h5>
+                <p class="card-text">Aqui su descripción</p>
+                <a href="#" class="btn btn-primary"> <i class="fas fa-info-circle"></i>  Mas información</a>
+            </div>
+        </div>
+        <div class="card-materias border" style="width: 18rem;">
+            <img class="card-img-top" src="https://www.britanico.edu.pe/blog/wp-content/uploads/2016/11/8-maneras-de-practicar-ingl%C3%A9s-en-clases-y-fuera-800x400.jpg" alt="Card image cap">
+            <div class="card-body text-center">
+                <h5 class="card-title">Ingles Técnico Avanzado I</h5>
+                <p class="card-text">Aqui su descripción</p>
+                <a href="#" class="btn btn-primary"> <i class="fas fa-info-circle"></i>  Mas información</a>
+            </div>
+        </div>
+        <div class="card-materias border" style="width: 18rem;">
+            <img class="card-img-top" src="https://www.dior.nl/wp-content/uploads/2015/09/scrum-board-1024x768.jpg" alt="Card image cap">
+            <div class="card-body text-center">
+                <h5 class="card-title">Metodología de Sistemas II</h5>
+                <p class="card-text">Aqui su descripción</p>
+                <a href="#" class="btn btn-primary"> <i class="fas fa-info-circle"></i>  Mas información</a>
+            </div>
+        </div>
+        <div class="card-materias border" style="width: 18rem;">
+            <img class="card-img-top" src="https://konclass.com/wp-content/uploads/2018/11/bases-de-datos.jpg" alt="Card image cap">
+            <div class="card-body text-center">
+                <h5 class="card-title">Base de datos II</h5>
+                <p class="card-text">Aqui su descripción</p>
+                <a href="#" class="btn btn-primary"> <i class="fas fa-info-circle"></i>  Mas información</a>
+            </div>
+        </div>
+    </div>
+    @include('layouts.footer')
     </body>
 </html>

d7f33e8da2ba911e0dde99f41bfbe81217211131
Cristian221995 <crisfonteina@gmail.com>
2019-05-31 16:33:03 -0300

    finish formmateria

--- resources/views/formmateria.blade.php
+++ resources/views/formmateria.blade.php
@@ -7,7 +7,7 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 </head>
 <body>
-
+    @include("layouts.navgrande")
     <div class="container">
         <form class="text-center border p-5" role="form" method="post" action="{{action('MateriaController@store')}}">
             {{csrf_field()}}

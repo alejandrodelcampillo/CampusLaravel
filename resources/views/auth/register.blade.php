7411adc0ce41e8935ee2c274d81250e60e2feb1e
Mauri05 <mauriagustin05@gmail.com>
2019-05-31 21:18:25 -0300

    fix views

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header text-center" style="font-weight: bold;font-size: 22px">{{ __('Register') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
